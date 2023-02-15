8331e5366fbdbfdc206fde16aa359404387b1424
athomicus <athomicus@gmail.com>
2023-02-15 19:26:36 +0100

    next files

--- Player.gd
+++ Player.gd
@@ -1,11 +1,13 @@
 extends KinematicBody2D
 #comit changest Test 05/02/2023 - Ar2r
 const ACCELERATION = 10
-const FRICTION = 30 # lower friction = more glide (ślizg) / mniejsze tarcie
+const FRICTION = 50 # lower friction = more glide (ślizg) / mniejsze tarcie
 
 const MAX_SPEED = 40
 var velocity = Vector2.ZERO
 onready var _animated_sprite = $AnimatedSprite
+onready var _animationTree = $AnimationTree
+onready var _animationState = _animationTree.get("parameters/playback")
 	
 
 func _physics_process(delta):
@@ -17,15 +19,12 @@ func _physics_process(delta):
 	
 	#below this  when our player moving:
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			_animated_sprite.play("RunRight")
-		else:
-			_animated_sprite.play("RunLeft")
-			
-			
+		_animationTree.set("parameters/Idle/blend_position",input_vector)
+		_animationTree.set("parameters/Run/blend_position",input_vector)
+		_animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED,ACCELERATION * delta )
 	else: #and when we no moving
-		#_animated_sprite.play("Idle")
+		_animationState.travel("Idle")  #_animated_sprite.play("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_collide(velocity)		
